938e28eb6bad9b9fb0cdc50f55a4a12d82800079
Morten Wulff <wulff@ratatosk.net>
2014-11-26 13:47:21 +0100

    Fixed paths in shell script

--- bin/sunburst.sh
+++ bin/sunburst.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-./sunburst.pl -i ../data/2003-expenses.csv -o ../html/data/2003.json -l 3
-./sunburst.pl -i ../data/2004-expenses.csv -o ../html/data/2004.json -l 3
-./sunburst.pl -i ../data/2005-expenses.csv -o ../html/data/2005.json -l 3
-./sunburst.pl -i ../data/2006-expenses.csv -o ../html/data/2006.json -l 3
-./sunburst.pl -i ../data/2007-expenses.csv -o ../html/data/2007.json -l 3
-./sunburst.pl -i ../data/2008-expenses.csv -o ../html/data/2008.json -l 3
-./sunburst.pl -i ../data/2009-expenses.csv -o ../html/data/2009.json -l 3
-./sunburst.pl -i ../data/2010-expenses.csv -o ../html/data/2010.json -l 3
-./sunburst.pl -i ../data/2011-expenses.csv -o ../html/data/2011.json -l 3
-./sunburst.pl -i ../data/2012-expenses.csv -o ../html/data/2012.json -l 3
-./sunburst.pl -i ../data/2013-expenses.csv -o ../html/data/2013.json -l 3
-./sunburst.pl -i ../data/2014-expenses.csv -o ../html/data/2014.json -l 3
-./sunburst.pl -i ../data/2015-expenses.csv -o ../html/data/2015.json -l 3
+./sunburst.pl -i ../data/expenses/2003-expenses.csv -o ../html/data/2003.json -l 3
+./sunburst.pl -i ../data/expenses/2004-expenses.csv -o ../html/data/2004.json -l 3
+./sunburst.pl -i ../data/expenses/2005-expenses.csv -o ../html/data/2005.json -l 3
+./sunburst.pl -i ../data/expenses/2006-expenses.csv -o ../html/data/2006.json -l 3
+./sunburst.pl -i ../data/expenses/2007-expenses.csv -o ../html/data/2007.json -l 3
+./sunburst.pl -i ../data/expenses/2008-expenses.csv -o ../html/data/2008.json -l 3
+./sunburst.pl -i ../data/expenses/2009-expenses.csv -o ../html/data/2009.json -l 3
+./sunburst.pl -i ../data/expenses/2010-expenses.csv -o ../html/data/2010.json -l 3
+./sunburst.pl -i ../data/expenses/2011-expenses.csv -o ../html/data/2011.json -l 3
+./sunburst.pl -i ../data/expenses/2012-expenses.csv -o ../html/data/2012.json -l 3
+./sunburst.pl -i ../data/expenses/2013-expenses.csv -o ../html/data/2013.json -l 3
+./sunburst.pl -i ../data/expenses/2014-expenses.csv -o ../html/data/2014.json -l 3
+./sunburst.pl -i ../data/expenses/2015-expenses.csv -o ../html/data/2015.json -l 3
